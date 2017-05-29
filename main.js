970800c9a40ae5c071251dd2ecf4049d9f479c1c
CobaltBlue-Team7 <obs0811@daum.net>
2017-05-29 22:43:15 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var mysql=require('mysql');
 var connection=mysql.createConnection({
 	host : 'localhost',
 	user : 'sensor2',
-	password : 'cse20121608',
+	password : 'Qw123456!@#$',
 	database : 'capstone'
 });
 
@@ -34,7 +34,7 @@ var server = http.createServer(function (req, resp) {
 	for (var i = 0; i < 50; i++)
 		redIndex[i] = 0;
 
-	console.log(req.url);
+	console.log("******* HERE ********" + req.url);
 	if (req.url == "/create") {
 	//	console.log(pgResp);
 		/*fs.readFile("./button.html", function (error, pgResp) {
@@ -66,13 +66,13 @@ var server = http.createServer(function (req, resp) {
 			if(err) {
 				throw err;
 			}
-			console.log("DB!");
+			//console.log("DB!");
 			connected = 1;
 			var cnt = 1;
 			for (var k in rows) {
-				console.log('rows['+k+'].amount_trash: '+rows[k].amount_trash);
-				if (rows[k].amount_trash > 50.0) {
-					console.log('****' + rows[k].floor + ':' + rows[k].bin_no);
+				//console.log('rows['+k+'].amount_trash: '+rows[k].amount_trash);
+				if (rows[k].amount_trash > 65.0) {
+					//console.log('****' + rows[k].floor + ':' + rows[k].bin_no);
 					redIndex[(rows[k].floor-1) * 10 + rows[k].bin_no] = 1;
 					var i = rows[k].floor * 10 + rows[k].bin_no;
 					//resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#f44336; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
@@ -85,18 +85,18 @@ var server = http.createServer(function (req, resp) {
 
 				}
 			}
-			console.log('****** Connected *******');
-			for (var i = 1; i <= 50; i++) {
-			console.log(redIndex[i]);
+			//console.log('****** Connected *******');
+			for (var i = 50; i >= 1; i--) {
+			//console.log(redIndex[i]);
 			if (redIndex[i] == 1) {
-				console.log('red');
+				//console.log('red');
 				resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#f44336; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 			}
 			else if (redIndex[i] == 0){
-				console.log('green');
+				//console.log('green');
 				resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#4CAF50; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 			}
-			if (i % 10 == 0)
+			if (i % 10 == 1)
 				resp.write('<br>');
 		}
 		resp.write('</div>');	
@@ -105,7 +105,7 @@ var server = http.createServer(function (req, resp) {
 		//	console.log('redIndex['+i+']: '+redIndex[i]);
 		//}
 		//
-		console.log('check redIndex[i]s');
+		//console.log('check redIndex[i]s');
 		/*for (var i = 1; i <= 50; i++) {
 			console.log(redIndex[i]);
 			if (redIndex[i] == 1) {
@@ -144,6 +144,16 @@ var server = http.createServer(function (req, resp) {
 
 	************************************************/
 	else if(str[0] == "/show_route") {
+		var begin = -1, end = -1;
+		var routeInput = req.url.split(':');
+
+		console.log(routeInput[0] + ' : ' + routeInput[1]);
+		if(routeInput[0].indexOf('?') != -1) {
+			var input = routeInput[0].split('?');
+			console.log(input[1] + ' : ' + routeInput[1]);
+			begin = input[1];
+			end = routeInput[1];
+		}
 		console.log('Routing');	
 		resp.write('<html><head><title>Routing</title></head><body>');
 		resp.write('routing');
@@ -164,59 +174,152 @@ var server = http.createServer(function (req, resp) {
 			if(err) {
 				throw err;
 			}
-			console.log("DB!");
+			//console.log("DB!");
 			connected = 1;
 			var cnt = 1;
 			for (var k in rows) {
-				console.log('rows['+k+'].amount_trash: '+rows[k].amount_trash);
-				if (rows[k].amount_trash > 50.0) {
-					console.log('****' + rows[k].floor + ':' + rows[k].bin_no);
-					redIndex[rows[k].floor * 10 + rows[k].bin_no] = 1;
-					var i = rows[k].floor * 10 + rows[k].bin_no;
+				//console.log('rows['+k+'].amount_trash: '+rows[k].amount_trash);
+				if (rows[k].amount_trash > 65.0) {
+				//	console.log('****' + rows[k].floor + ':' + rows[k].bin_no);
+					redIndex[(rows[k].floor-1) * 10 + rows[k].bin_no] = 1;
+					var i = (rows[k].floor-1) * 10 + rows[k].bin_no;
 					//resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#f44336; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 
 				}
 				else {
-					redIndex[rows[k].floor * 10 + rows[k].bin_no] = 0;
-					var i = rows[k].floor * 10 + rows[k].bin_no;
+					redIndex[(rows[k].floor-1) * 10 + rows[k].bin_no] = 0;
+					var i = (rows[k].floor-1) * 10 + rows[k].bin_no;
 					//resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#4CAF50; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 
 				}
 			}
-			console.log('****** Connected *******');
-			for (var i = 1; i <= 50; i++) {
-			console.log(redIndex[i]);
+			//console.log('****** Connected *******');
+			for (var i = 50; i >= 1; i--) {
+			//console.log(redIndex[i]);
 			if (redIndex[i] == 1) {
-				console.log('red');
+			//	console.log('red');
 				resp.write('<button id="b' + i + '" onclick="button_click('+ i + ', ' + redIndex[i] + ');" style="background-color:#f44336; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 			}
 			else if (redIndex[i] == 0){
-				console.log('green');
+			//	console.log('green');
 				resp.write('<button id="b' + i + '" onclick="button_click('+ i + ', ' + redIndex[i] + ');" style="background-color:#4CAF50; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 			}
-			if (i % 10 == 0)
+			if (i % 10 == 1)
 				resp.write('<br>');
-		}
-		resp.write('</div>');	
-		});
-		//for(var i = 1; i <= 50; i++) {
-		//	console.log('redIndex['+i+']: '+redIndex[i]);
-		//}
-		//
-		console.log('check redIndex[i]s');
-		/*for (var i = 1; i <= 50; i++) {
-			console.log(redIndex[i]);
-			if (redIndex[i] == 1) {
-				resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#f44336; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
 			}
-			else if (redIndex[i] == 0){
-				resp.write('<button id="b' + i + '" onclick="button_click('+ i + ');" style="background-color:#4CAF50; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
-			}
-			if (i % 10 == 0)
-				resp.write('<br>');
-		}
-		resp.write('</div>');	*/
+			resp.write('</div>');
+			if(begin != -1 && end != -1) {	// start routing
+				console.log('Begin, End!');
+				var yPos = new Array(51);
+				var xPos = new Array(51);
+				var binNum = 1;
+				yPos[0] = Math.floor((begin-1) / 10 + 1);
+				xPos[0] = (begin-1) % 10 + 1;
+				for(var i = 1; i <= 50; i++) {
+					if(i == begin || i == end)	continue;
+					if(redIndex[i] == 1) {
+						yPos[binNum] = Math.floor((i-1) / 10 + 1);
+						xPos[binNum++] = (i-1) % 10 + 1;
+					}
+				}
+				yPos[binNum] = Math.floor((end-1) / 10 + 1);
+				xPos[binNum++] = (end-1) % 10 + 1;
+				for(var i = 0; i < binNum; i++) {
+					console.log(yPos[i] + ' ' + xPos[i]);
+				}
+				var adjMatrix = new Array(binNum);
+				var cache = new Array(binNum);
+				var bestChoice = new Array(binNum);
+				var path = new Array(binNum);
+				var cntPath = 0, sumCost = 0, cnt = 0, INF = 9999999, floorCost = 9;
 
+				distance = function(y1, x1, y2, x2) {
+					if(y1 != y2)	
+						return Math.abs(y1-y2) * floorCost + x1 + x2;
+					else
+						return Math.abs(x1-x2);
+				}
+				TSP = function(cur, visited) {
+					if(visited == (1 << binNum) - 1)	return 0;
+					if(cache[cur][visited] != 0)	return cache[cur][visited];
+					cache[cur][visited] = INF;
+					for(var next = 0; next < binNum; next++) {
+						if(visited & (1 << next))	continue;
+						if(adjMatrix[cur][next] == 0)	continue;
+						var cost = TSP(next, visited | (1 << next)) + adjMatrix[cur][next];
+						if(cost < cache[cur][visited]) {
+							cache[cur][visited] = cost;
+							bestChoice[cur][visited] = next;
+						}
+					}
+					return cache[cur][visited];
+				}
+				findPath = function(cur, visited) {
+					if(visited == (1 << binNum) - 1)	return;
+					var next = bestChoice[cur][visited];
+					path[cntPath++] = next;
+					sumCost += adjMatrix[cur][next];
+					findPath(next, visited | (1 << next));
+				}
+				for(var i = 0; i < binNum; i++) {
+					adjMatrix[i] = new Array(binNum);
+					adjMatrix[i][i] = 0;
+				}
+				for(var i = 0; i < binNum; i++) {
+					cache[i] = new Array(65536);
+					bestChoice[i] = new Array(65536);
+					for(var j = 0; j < 65536; j++) {
+						cache[i][j] = 0;
+						bestChoice[i][j] = 0;
+					}
+				}
+				
+				for(var i = 0; i < binNum-1; i++) {
+					for(var j = i+1; j < binNum; j++) {
+						var dist = distance(yPos[i], xPos[i], yPos[j], xPos[j]);
+						adjMatrix[i][j] = adjMatrix[j][i] = dist;
+					}
+				}
+				var ans = TSP(0, 1);
+				console.log('ans: ', ans);
+				path[0] = 0;
+				cntPath = 1;
+				findPath(0, 1);
+				console.log("path: ");
+				for(var i = 0; i < cntPath; i++) {
+					console.log(path[i]);
+				}
+				console.log("cost: ", sumCost);
+				
+				resp.write('<div style="padding-top: 100px; padding-left: 100px">');
+				
+				var pathIndex = new Array(51);
+				for (var i = 0; i < binNum; i++) {
+					pathIndex[i] = (yPos[i] - 1) * 10 + xPos[i];
+				}
+
+				for (var i = 50; i >= 1; i--) {
+					var flag = -1;
+					for (var j = 0; j < binNum; j++) {
+						if (pathIndex[j] == i)
+							flag = j + 1;
+					}
+
+					if (flag > 0) {
+						resp.write('<button id="b' + i + '" onclick="button_click('+ i + ', ' + redIndex[i] + ');" style="color:white; font-size: 30pt; background-color:#0066CC; height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> ' + flag + '</button>');
+					}
+					else {
+						resp.write('<button id="b' + i + '" onclick="button_click('+ i + ', ' + redIndex[i] + ');" style="height: 120px; width: 80px; margin: 20px 10px 20px 10px; padding: 50px 30px 50px 30px"> Bin ' + i + '</button>');
+					}
+					if (i % 10 == 1)
+						resp.write('<br>');
+				}	
+			
+				resp.write('</div>');
+
+			}
+		});
+	
 		resp.write('<button id="route" onclick="button_route(51);" style="height: 150px; width: 150px; margin: 20px 10px 20px 10px; padding: 100px 30px 50px 100px"> Route </button><br>');
 
 		resp.write('<script language="javascript"> var buttonClicked = 0, begin = -1, end = -1;');
@@ -227,121 +330,50 @@ var server = http.createServer(function (req, resp) {
 						  begin = s; buttonClicked = 1; \
 						} else { end = s; } } \
 				  alert("begin: " + begin + " end: " + end);\
-				}');
-
-		resp.write('\
-			var yPos = new Array(1, 1, 2); \
-			var xPos = new Array(1, 2, 1); \
-			var binNum = xPos.length+1;	\
-			yPos[binNum-1] = 1;		\
-			xPos[binNum-1] = 0;		\
-			var binAdjMatrix = new Array(binNum);		\
-			var cache = new Array(binNum);				\
-			var bestChoice = new Array(binNum);			\
-			var path = new Array(binNum);				\
-			var cntPath = 0;							\
-			var sumCost = 0;							\
-			var cnt = 0;								\
-			var FLOOR_COST = 100;						\
-			var INFINITE = 9999999;						\
-			var nodeVisited = new Array(binNum);	\
-			Distance = function(firstBin_Ypos, firstBin_Xpos, secondBin_Ypos, secondBin_Xpos) {		\
-				if (firstBin_Ypos != secondBin_Ypos) {		\
-					return Math.abs(firstBin_Ypos - secondBin_Ypos) * FLOOR_COST + firstBin_Xpos + secondBin_Xpos;	\
-				}		\
-				else {	\
-					return Math.abs(firstBin_Xpos - secondBin_Xpos);	\
-				}	\
-			} </script>');
-		/*
-			shortestPath = function(cur, cnt){
-				var ret = INFINITE, cost;	\
-				if(cnt == binNum - 2) {	\
-					bestChoice[cur][cnt] = binNum-1;		\
-					return binAdjMatrix[cur][binNum-1];		\
-				}				\
-				if(cache[cur][cnt]) {		\
-					return cache[cur][cnt];	\
-				}		\
-				for(var next = 0; next <= binNum-2; next++) {	\
-					if(!nodeVisited[next]) {					\
-						nodeVisited[next] = 1;					\
-						cost = binAdjMatrix[cur][next] + shortestPath(next, cnt+1);		\
-						if(cost < ret) {			\
-							ret = cost;				\
-							bestChoice[cur][cnt] = next;	\
-						}			\
-						nodeVisited[next] = 0;	\
-					}	\
-				}	\
-				cache[cur][cnt] = ret;	\
-				return ret;	\
-			}	*/
-			
-			//resp.write('findPath = function(cur, cnt) {\
-			//	if(cnt == binNum-1)\
-			//		return;\
-			//	var next = bestChoice[cur][cnt];\
-			//	path[cntPath++] = next;\
-			//	console.log("cur: ", cur, " cnt: ", cnt, " next: ",  next);\
-			//	console.log("cost: ", binAdjMatrix[cur][next]);\
-			//	sumCost += binAdjMatrix[cur][next];\
-			//	findPath(next, cnt+1);\
-			//};	\
-			//for (var i = 0; i < binNum; i++) {	\
-				//binAdjMatrix[i] = new Array(binNum);\
-				//binAdjMatrix[i][i] = 0;\
-			//} \
-			/*
-			for (var i = 0; i < binNum; i++) {\
-				nodeVisited[i] = 0;				\
-				cache[i] = new Array(65536);	\
-				bestChoice[i] = new Array(65536);	\
-				for (var j = 0; j < 65536; j++) {	\
-					cache[i][j] = 0;				\
-					bestChoice[i][j] = 0;			\
-				}									\
-			}		\
-			console.log("Raw input:");		\
-			for (var i = 0; i < binNum; i++) {	\
-				console.log("(", yPos[i], ",", xPos[i], ")");	\
-			}	\
-			for (var i = 0; i < binNum - 1; i++) {	\
-				for (var j = i + 1; j < binNum; j++) {	\
-					var distance = Distance(yPos[i], xPos[i], yPos[j], xPos[j]);	\
-					binAdjMatrix[i][j] = distance;	\
-					binAdjMatrix[j][i] = distance;	\
-				}	\
-			}	\
-			console.log("\nAdjacent Matrix:");	\
-			for (var i = 0; i < binNum; i++) {	\
-				console.log(binAdjMatrix[i][0], binAdjMatrix[i][1], binAdjMatrix[i][2], binAdjMatrix[i][3]);	\
-			}	\
-			nodeVisited[0] = 1;	\
-			console.log("\nCalculating the optimal cost");	\
-			var answer = shortestPath(0, 0);		\
-			console.log("total cost: ", answer);	\
-			console.log("\nFinding the optimal path");		\
-			path[cntPath++] = 0;		\
-			findPath(0, 0);				\
-			console.log("\ncntPath: ", cntPath);	\
-			console.log("path: ");					\
-			for (var i = 0; i < cntPath; i++) {		\
-				console.log(path[i]);				\
-			}		\
-			console.log("cost: ", sumCost);		\
-			for(var i = 0; i < cntPath; i++) {	\
-				var idx = path[i];		\
-				console.log("%d: (%d, %d)", i+1, yPos[idx], xPos[idx]);	\
-			}	\
-		</script> ');*/
-
+				  if(begin != -1 && end != -1) {	\
+					location.href = "/show_route?"+begin+":"+end;	\
+				  }	\
+				} </script>');
+		//var sample = require("./sample");
+		//resp.write('<script> console.log("Please");	</script>');
+		//resp.write('<script> var fs=require("fs"); fs.writeFileSync("./input.txt", "Hello", "utf8"); </script>');
+		//fs.writeFileSync("./input.txt", "Hello", "utf8");
+		//var input = fs.readFileSync('input.txt', 'utf8');
+		//console.log(input);
+		//sample.a();
+		//resp.write('<script language="javascript" src="routing.js"> </script>'); 
+		/*var buttonClicked = 0, begin = -1, end = -1;');
+		resp.write('function button_click(s, red) \
+		{ if(!red) { alert("Click a red bin"); } \
+				  else { alert("Fine");  \
+					  if(!buttonClicked) { \
+						  begin = s; buttonClicked = 1; \
+						} else { end = s; } } \
+				  alert("begin: " + begin + " end: " + end);\
+		}');*/
+		//resp.write('');
+		//console.log('begin: ' + begin + 'end: ' + end);
+		/*if(begin != -1 && end != -1) {	// start routing
+			console.log('Begin, End!');
+			var yPos = new Array(51);
+			var xPos = new Array(51);
+			var binNum = 0;
+			for(var i = 1; i <= 50; i++) {
+				if(redIndex[i] == 1) {
+					yPos[binNum] = (i-1) / 10 + 1;
+					xPos[binNum++] = (i-1) % 10 + 1;
+				}
+			}
+			for(var i = 0; i < binNum; i++) {
+				console.log(yPos[i] + ' ' + xPos[i]);
+			}
+		}*/
 		resp.write('<script language="javascript">');
 		resp.write('function button_route(s) { location.href="/show_route"}</script>');
 		resp.write('</body></html>');
 	}
 	else if(str[0] =="/show_graph") {
-			var floor = str[1].split(':');
+		var floor = str[1].split(':');
 			console.log(floor[0]);
 			console.log(floor[1]);
 			var html = fs.readFile("./graph1.html", function (error, html) {
@@ -396,7 +428,6 @@ var server = http.createServer(function (req, resp) {
 			});
 		});
 	}
-	
 	else {
 		resp.writeHead(200, { 'Content-Type': 'text/html' });
 		resp.write('<h1>Product Manager</h1><br /><br />To create product please enter: ' + req.url);
